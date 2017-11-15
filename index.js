18ec0f7bca688956ab501d1024dd12171dd153a3
Ryan Heap <rheap@degdigital.com>
2017-11-14 22:47:31 -0600

    initial API footprint

---
+++ index.js
@@ -0,0 +1,28 @@
+const skeletorPlugin = () => {
+
+	/* 
+		API Method: run
+		Description: Runs plugin
+		Returns: promise
+		
+		Parameter: config
+		Type: string|object|function
+		Required: true
+		Example: () => 'TBD'
+
+		Parameter: options
+		Type: object
+		Required: false
+		Example: { debug: true }
+	*/
+
+	const run = (config, options) => {
+
+	}
+
+	return {
+		run
+	};
+};
+
+module.exports = skeletorPlugin;
